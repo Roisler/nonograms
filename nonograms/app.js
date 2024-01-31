ceed3f07eee1b758b4a05de09de5b8bb29a0d546
Sergey Bondarchuk <roislerxs@gmail.com>
2024-01-31 09:55:57 +0700

    feat: remove canvas

--- nonograms/app.js
+++ nonograms/app.js
@@ -1,19 +1,39 @@
-const canvas = document.getElementById('cells');
 const cellsContainer = document.querySelector('.cells');
+const collsContainer = document.querySelector('.cols');
+const rowsContainer = document.querySelector('.rows');
 
-for (let i = 1; i <= 25; i += 1) {
+const initialGame = {
+  complexity: 10,
+};
+
+const { complexity } = initialGame;
+
+for (let i = 1; i <= complexity ** 2; i += 1) {
   const cell = document.createElement('div');
   cell.classList.add('cell');
   cell.setAttribute('id', i);
   cellsContainer.append(cell);
 }
 
-const ctx = canvas.getContext('2d');
+rowsContainer.classList.add(`row-${complexity}`);
+collsContainer.classList.add(`col-${complexity}`);
+
+for (let i = 1; i <= complexity; i += 1) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  const col = document.createElement('div');
+  col.classList.add('col');
+
+  rowsContainer.append(row);
+  collsContainer.append(col);
+}
+
+/*const ctx = canvas.getContext('2d');
 
 canvas.width = cellsContainer.offsetWidth;
-canvas.height = cellsContainer.offsetHeight;
+canvas.height = cellsContainer.offsetHeight;*/
 
-const cells = document.querySelectorAll('.cell');
+/*const cells = document.querySelectorAll('.cell');
 cells.forEach((cell) => {
   cell.addEventListener('click', (e) => {
     const elementHeight = e.target.offsetHeight;
@@ -29,7 +49,7 @@ cells.forEach((cell) => {
     ctx.lineTo(elementWidth * (cell - 1), elementHeight * (row - 1))
     ctx.fill();
   })
-})
+})*/
 
 
 
